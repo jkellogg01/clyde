99ab10f6a52f6e4aa0678c14466d0c8522f32cff
Joshua Kellogg <kellogg.20.joshua@gmail.com>
2024-06-16 17:20:29 -0600

    reset to somewhat of a default state
    I decided I'm going about this all wrong. going back on my work so that I can build some elements out one at a time.

--- main.go
+++ main.go
@@ -4,21 +4,12 @@ import (
 	"log"
 
 	tea "github.com/charmbracelet/bubbletea"
-	"github.com/charmbracelet/lipgloss"
 )
 
 type model struct {
 	width  int
 	height int
-	active applicationStatus
 }
-
-type applicationStatus int
-
-const (
-	form applicationStatus = iota
-	results
-)
 
 func New() *model {
 	m := new(model)
@@ -31,46 +22,11 @@ func (m model) Init() tea.Cmd {
 
 func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	var cmd tea.Cmd
-	switch msg := msg.(type) {
-	case tea.WindowSizeMsg:
-		m.width = msg.Width
-		m.height = msg.Height
-	case tea.KeyMsg:
-		switch msg.String() {
-		case "enter":
-			if m.active == form {
-				m.active = results
-			}
-		case "esc":
-			if m.active == results {
-				m.active = form
-			}
-		case "ctrl+c", "q":
-			return m, tea.Quit
-        default:
-            log.Print(msg.String())
-		}
-	}
-	return m, cmd
+    return m, cmd
 }
 
 func (m model) View() string {
-	var active string
-	switch m.active {
-	case form:
-		active = "form mode"
-	case results:
-		active = "results mode"
-	default:
-		panic("invalid model state")
-	}
-	return lipgloss.Place(
-		m.width,
-		m.height,
-		0.5,
-		0.5,
-		active,
-	)
+    return ""
 }
 
 func main() {
