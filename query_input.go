c17eadebd662431d5c8eb6827b43477639f1066b
Joshua Kellogg <kellogg.20.joshua@gmail.com>
2024-07-04 13:37:22 -0600

    actual input validation with debouncing and everything! it's like i'm writing real software or something

--- query_input.go
+++ query_input.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 	"log"
 	"net/url"
+	"time"
 
 	"github.com/charmbracelet/bubbles/textinput"
 	tea "github.com/charmbracelet/bubbletea"
@@ -13,14 +14,14 @@ import (
 type QueryInput struct {
 	value   string
 	infield textinput.Model
-	err     string
+	err     error
 }
 
 func NewQueryInput() *QueryInput {
 	m := new(QueryInput)
 	m.infield = textinput.New()
 	m.infield.Prompt = ""
-	m.infield.Placeholder = "query..."
+	m.infield.Placeholder = "https://www.google.com/search?q=something"
 	m.infield.Focus()
 	return m
 }
@@ -30,18 +31,28 @@ var (
 	ErrUnsupportedProtocol = errors.New("this protocol is not supported!")
 )
 
-func (m QueryInput) segments() ([]string, error) {
-	result := make([]string, 0)
-	result = append(result, m.infield.View())
-	if len(m.value) < 5 {
-		return result, ErrIncompleteQuery
+func (m QueryInput) validate() error {
+	log.Printf("parsing input: %s", m.value)
+	parsed, err := url.Parse(m.value)
+	if err != nil {
+		return err
 	}
-	return result, nil
+    log.Printf("found scheme '%s' and hostname '%s'", parsed.Scheme, parsed.Hostname())
+	if parsed.Scheme == "" || parsed.Hostname() == "" {
+		return ErrIncompleteQuery
+	}
+	if parsed.Scheme != "http" && parsed.Scheme != "https" {
+		return ErrUnsupportedProtocol
+	}
+    log.Printf("the url '%s' is valid!", parsed.String())
+	return nil
 }
 
 func (m QueryInput) Init() tea.Cmd {
 	return nil
 }
+
+type validateMsg string
 
 func (m QueryInput) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	var cmd tea.Cmd
@@ -49,31 +60,40 @@ func (m QueryInput) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	case tea.WindowSizeMsg:
 		m.infield.Width = msg.Width - 3
 	case tea.KeyMsg:
-		if msg.String() == "enter" {
+		switch msg.String() {
+		case "enter":
 			// right now this is just so that the list of tokens gets logged to the console
-            parsed, err := url.Parse(m.value)
-            if err != nil {
-                log.Printf("ERROR: %s", err)
-            }
-            log.Printf("Parsed URL: %s", parsed.String())
+			parsed, err := url.Parse(m.value)
+			if err != nil {
+				log.Printf("ERROR: %s", err)
+			}
+			log.Printf("Parsed URL: %s", parsed.String())
+		default:
+			m.infield, _ = m.infield.Update(msg)
+			return m, tea.Tick(time.Second, func(t time.Time) tea.Msg {
+				return validateMsg(m.value)
+			})
+		}
+	case validateMsg:
+		if string(msg) == m.value {
+			m.err = m.validate()
 		}
 	}
-	m.infield, cmd = m.infield.Update(msg)
 	m.value = m.infield.Value()
 	return m, cmd
 }
 
 func (m QueryInput) View() string {
-	segments, err := m.segments()
-	if err != nil {
-		m.err = err.Error()
+	var inputErr string
+	if m.err != nil {
+		inputErr = m.err.Error()
 	}
 	return lipgloss.JoinVertical(
 		lipgloss.Top,
 		lipgloss.NewStyle().Border(lipgloss.NormalBorder()).Render(lipgloss.JoinHorizontal(
 			lipgloss.Left,
-			segments...,
+			m.infield.View(),
 		)),
-		lipgloss.NewStyle().Foreground(lipgloss.ANSIColor(1)).Render(m.err),
+		lipgloss.NewStyle().Foreground(lipgloss.ANSIColor(1)).Render(inputErr),
 	)
 }
